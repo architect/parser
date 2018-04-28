9e64e8baabeee8dc83ec906f74251e7937d83b8d
Brian LeRoux <b@brian.io>
2018-04-28 13:24:53 -0500

    fix old bugs with funky comments; should strip everything now

--- _remove-comments.js
+++ _remove-comments.js
@@ -1,6 +1,16 @@
 /**
- * removes # comments and trims leading/trailing whitespace
+ * removes:
+ * - comments
+ * - empty lines
+ * - trailing spaces
  */
+var comments = /\#.*/gm
+var emptyLines = /(^[ \t]*\n)/gm
+var trailingSpaces = / (\n|\r)/g
+
 module.exports = function removeComments(text) {
-  return text.trim().replace(/\#.*/g, '').replace(/  \n/g, '').trim()
+  var noComments = text.trim().replace(comments, '').trim()
+  var noEmptyLines = noComments.replace(emptyLines, '')
+  var noTrailingSpaces = noEmptyLines.replace(trailingSpaces, '\n')
+  return noTrailingSpaces
 }
---
+++ test/03-comments-test.js
@@ -0,0 +1,12 @@
+var test = require('tape')
+var fs = require('fs')
+var _remove = require('../_remove-comments')
+
+test('removed comments', t=> {
+  t.plan(1)
+  var mock = fs.readFileSync('./test/02-mock-comments-arc').toString()
+  var parsed = _remove(mock)
+  t.ok(parsed, 'failed on bad input')
+  console.log(parsed)
+
+})

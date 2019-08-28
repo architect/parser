feba0b4bb631472e77adbd798023e3d0e00441dc
kj <kristoferjoseph@gmail.com>
2019-08-28 09:38:05 -0700

    Initial commit to create stringify feature branch.

--- test/07-stringify-test.js
+++ test/07-stringify-test.js
@@ -2,7 +2,7 @@ var test = require('tape')
 var fs = require('fs')
 var parse = require('../')
 
-test('@scheduled', t=> {
+test('@stringify', t=> {
   t.plan(1)
   var mock = fs.readFileSync('./test/05-mock-arc.json').toString()
   var parsed = parse.yaml.stringify(mock)

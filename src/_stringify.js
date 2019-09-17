8cd69be4ac7d12a18f1c9253182a97c6a720a731
Mike MacCana <mike.maccana@gmail.com>
2019-09-17 12:05:41 +0100

    Tighten

--- src/_stringify.js
+++ src/_stringify.js
@@ -3,8 +3,7 @@ module.exports = function stringify (obj) {
 
   let keys = Object.keys(obj)
   let out = ''
-  for (let i = 0; i < keys.length; i++) {
-    let pragma = keys[i]
+  keys.forEach(function (pragma, i) {
     let props = obj[pragma]
     out += i === 0
       ? `@${pragma}\n`
@@ -34,6 +33,6 @@ module.exports = function stringify (obj) {
         }
       }
     }
-  }
+  })
   return out
 }

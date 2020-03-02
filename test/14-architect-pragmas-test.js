06cfd5d64b4cfee0337a0a5dada11737e82d10af
Ryan Block <rblock@gmail.com>
2020-03-01 16:40:43 -0800

    Update tests for official Architect manifest pragmas

---
+++ test/14-architect-pragmas-test.js
@@ -0,0 +1,39 @@
+let test = require('tape')
+let fs = require('fs')
+let parse = require('../')
+
+test('Test full Architect project manifest mock', t=> {
+  let mock = fs.readFileSync('./test/mock/arc.arc').toString()
+  let arc = parse(mock)
+  t.ok(arc, 'parsed mock')
+  let pragmas = [
+    'app',
+    'aws',
+    'static',
+    'http',
+    'ws',
+    'events',
+    'scheduled',
+    'queues',
+    'tables',
+    'indexes'
+  ]
+  let pragmaConfig = {
+    app: 1,
+    aws: 4,
+    static: 2,
+    http: 5,
+    ws: 0,
+    events: 2,
+    scheduled: 1,
+    queues: 3,
+    tables: 2,
+    indexes: 2
+  }
+  t.plan(1 + (pragmas.length * 2))
+  let isArray = i => Array.isArray(i)
+  pragmas.forEach(pragma => {
+    t.ok(isArray(arc[pragma]), `${pragma} parsed to array`)
+    t.equal(arc[pragma].length, pragmaConfig[pragma], `${pragma} has ${arc[pragma].length} items`)
+  })
+})
---
+++ test/mock/arc.arc
@@ -0,0 +1,49 @@
+@app
+testapp
+
+@aws
+runtime nodejs10.x
+region us-west-1
+profile work
+bucket mahbukkitt
+
+@static
+fingerprint true
+folder dist
+
+@http
+get    /
+post   /post
+put    /put
+patch  /patch
+delete /delete
+
+@ws
+
+@events
+an-event
+another-event
+
+@scheduled
+daily-foo rate(1 day)
+
+@queues
+a-queue
+another-queue
+so-many-queues
+
+@tables
+data
+  ID *String
+  key **String
+
+moreData
+  ID *String
+  key **String
+
+@indexes
+data
+  anotherID *Number
+
+moreData
+  anotherID *Number

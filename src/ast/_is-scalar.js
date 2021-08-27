5b7523852df19eb4bb680fc90bd7860a990c8d96
Brian LeRoux <b@brian.io>
2021-08-27 15:34:09 -0700

    chill

--- src/ast/_is-scalar.js
+++ src/ast/_is-scalar.js
@@ -2,19 +2,6 @@ let getLines = require('./_get-lines')
 let notempty = require('./_not-empty')
 
 module.exports = function isScalar (working) {
-
-  /*
-  // get the indices of all newlines
-  let newlines = working.map((t, i) => t.type === 'newline' ? i : false).filter(Boolean)
-
-  // get collection of lines: [[{token}, {token}], [{token, token}]]
-  let lines = newlines.reduce(function linebreak (collection, newline, index) {
-    let start = index === 0 ? index : newlines[index - 1] + 1
-    collection.push(working.slice(start, newline))
-    return collection
-  }, [])
-  */
-  let lines = getLines(working)
-  // is the first line a scalar value
-  return lines[0].filter(notempty).length === 1
+  let first = getLines(working)[0]
+  return first.filter(notempty).length === 1
 }

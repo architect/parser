40cd9f3485e7c59b75ef51050396fb1be096b24a
Brian LeRoux <b@brian.io>
2021-09-09 09:20:26 -0700

    try this

--- src/parser/get-type.js
+++ src/parser/get-type.js
@@ -45,6 +45,8 @@ module.exports = function type ({ tokens, index }) {
   let scalar = isSingle(working)
   let indent = isIndent(working)
 
+  console.log({ scalar, indent })
+
   if (scalar && indent === false)
     return { end: 1, value: { ...tokens[index] } }
 

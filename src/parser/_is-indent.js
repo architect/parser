0a1b11019fe8a51855ddfbc623150d28f710d593
Brian LeRoux <b@brian.io>
2021-09-09 11:16:21 -0700

    reductio ad absurdum

--- src/parser/_is-indent.js
+++ src/parser/_is-indent.js
@@ -2,5 +2,6 @@ let getLines = require('./_get-lines')
 
 module.exports = function isIndented (working) {
   let second = getLines(working)[1]
+  console.log({ second })
   return Array.isArray(second) && second.length >= 3 && second[0].type === 'space' && second[1].type === 'space'
 }

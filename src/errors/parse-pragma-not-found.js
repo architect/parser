226a5b54b37ad97b6cb6b56d2ef1fba4d29651ff
Ryan Block <rblock@gmail.com>
2022-02-02 08:56:13 -0800

    Update PragmaNotFound description Move to npm `files` property, remove some cruft from the repo

--- src/errors/parse-pragma-not-found.js
+++ src/errors/parse-pragma-not-found.js
@@ -1,6 +1,6 @@
 module.exports = class PragmaNotFound extends ReferenceError {
   constructor () {
-    super(`@pragma not found`)
+    super(`Architect files must contain at least one pragma`)
     this.name = this.constructor.name
   }
 }
